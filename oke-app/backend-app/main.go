3f5ac4746e725853aad6f365ba304d5645a5eae8
tniita <takuya2230@gmail.com>
2023-07-10 10:56:26 +0900

    WIP

--- oke-app/backend-app/main.go
+++ oke-app/backend-app/main.go
@@ -13,15 +13,12 @@ func main() {
 	db.SetupDB()
 	router := gin.Default()
 	router.Use(cors.New(cors.Config{
-		// アクセスを許可したいアクセス元
 		AllowOrigins: []string{"*"},
-		// アクセスを許可したいHTTPメソッド(以下の例だとPUTやDELETEはアクセスできません)
 		AllowMethods: []string{
 			"POST",
 			"GET",
 			"DELETE",
 		},
-		// 許可したいHTTPリクエストヘッダ
 		AllowHeaders: []string{
 			"Access-Control-Allow-Credentials",
 			"Access-Control-Allow-Headers",
@@ -30,10 +27,8 @@ func main() {
 			"Accept-Encoding",
 			"Authorization",
 		},
-		// cookieなどの情報を必要とするかどうか
 		AllowCredentials: true,
-		// preflightリクエストの結果をキャッシュする時間
-		MaxAge: 24 * time.Hour,
+		MaxAge:           24 * time.Hour,
 	}))
 	router.GET("/items", http.GetAll)
 	router.GET("/items/:id", http.GetItemById)
